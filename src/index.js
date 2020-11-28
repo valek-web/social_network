6dad0be88f63819d5eb52a85e50e86121423cda1
Valek <ziryanovvalentin@mail.ru>
2020-11-28 10:30:36 +0300

    shcol

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import * as serviceWorker from './serviceWorker';
-import state, { newPostClick, updatePostText, renderNew } from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import stone from './redux/state';
 
 export let renderAll = (states) => {
-  debugger
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state}  newPostClick={newPostClick} updatePostText={updatePostText}/>
+      <App state={states}  newPostClick={stone.newPostClick.bind(stone)} updatePostText={stone.updatePostText.bind(stone)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-renderAll(state)
-debugger
-renderNew(renderAll)
-debugger
+renderAll(stone.getState())
+stone.renderNew(renderAll)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,38 +1,39 @@
-let renderNewChange = (sts) => {}
+let stone = {
+   _state: {
+      profilePage:{
+          post:[{ messag: 'New post!', like: 'like: 2' },
+                { messag: "I'm learn Java!", like: 'like: 5' },
+                { messag: "I'm learn Python!", like: 'like: 33'},
+                { messag: 'I want programmes!', like: 'like: 42' }],
+          newPostText: 'New pos11t!'
+       },
+      messagesPage: [
+               {name:'Jonson', id:'0'},
+               {name:'Li', id:'1'},
+               {name:'Max', id:'2'},
+               {name:'Linda', id:'3'},
+               {name:'Ron', id:'4'},]},
 
-let state = {
-     profilePage:{
-         post:[{ messag: 'New post!', like: 'like: 2' },
-               { messag: "I'm learn Java!", like: 'like: 5' },
-               { messag: "I'm learn Python!", like: 'like: 33'},
-               { messag: 'I want programmes!', like: 'like: 42' }],
-         newPostText: 'New pos11t!'
-      },
-     messagesPage: [
-              {name:'Jonson', id:'0'},
-              {name:'Li', id:'1'},
-              {name:'Max', id:'2'},
-              {name:'Linda', id:'3'},
-              {name:'Ron', id:'4'},]
- }
+   getState() {
+      return this._state;
+   },
 
- window.state = state;
+   newPostClick() {
+      let newTextPosts = this._state.profilePage.newPostText;
+      let newPosting = {messag: newTextPosts, like: 'Like: 0'};
+      this._state.profilePage.post.push(newPosting);
+      this._state.profilePage.newPostText = '';
+      this.renderNewChange(this._state);},
 
-export let newPostClick = () => {
-   let newTextPosts = state.profilePage.newPostText;
-   let newPosting = {messag: newTextPosts, like: 'Like: 0'};
-   state.profilePage.post.push(newPosting);
-   state.profilePage.newPostText = '';
-   renderNewChange(state);
+   updatePostText(newText) {
+      debugger
+      this._state.profilePage.newPostText = newText;
+      debugger
+      this.renderNewChange(this._state);},
+
+   renderNewChange(sts) {},
+
+   renderNew(rend) {this.renderNewChange = rend;},
 }
 
-export let updatePostText = (newText) => {
-   state.profilePage.newPostText = newText;
-   renderNewChange(state);
-}
-
-export let renderNew = (rend) => {
-   renderNewChange = rend;
-}
-
- export default state;
+ export default stone;
