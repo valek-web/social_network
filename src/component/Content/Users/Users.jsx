4d8a8279024ea01f3e29ce884b3320db6d0a7386
Valek <ziryanovvalentin@mail.ru>
2021-08-06 01:42:54 +0300

    lesson 55++

--- src/component/Content/Users/Users.jsx
+++ src/component/Content/Users/Users.jsx
@@ -16,9 +16,10 @@ class Users extends React.Component {
         }
     }
 
-    onPageClick = (pageNum) => {
-        this.props.setCurrentPage(pageNum)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.onPageSize}&page=${pageNum}`)
+    onPageClick = (bool, pageNum = this.props.onCurrentPage) => {
+        let currentPageNum = bool ? pageNum + 1 : pageNum - 1
+        this.props.setCurrentPage(currentPageNum)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.onPageSize}&page=${currentPageNum}`)
             .then(respons => {
                 this.props.setUsers(respons.data.items)
             }
@@ -39,22 +40,18 @@ class Users extends React.Component {
             )
         })
 
-        let amountPage = Math.ceil(this.props.onTotalUsersCount / this.props.onPageSize)
-        let arrayPage = []
-        for (let index = 1; index <= amountPage; index++) {
-            arrayPage.push(index)
-        }
+        let current = this.props.onCurrentPage
+        let total = this.props.onTotalUsersCount
 
-        let mapArryaPage = arrayPage.map(i => {
-            return (
-                <span className={i == this.props.onCurrentPage ? s.active : s.spn}
-                    onClick={() => this.onPageClick(i)}
-                    key={i}>{i}</span>
-            )
-        })
         return (
             <div>
-                <div className={s.countPage}>{mapArryaPage}</div>
+                <div className={s.countPage}>
+                    <div className={s.box_button}>
+                        <div className={current == 1 ? s.block : s.btn_next} onClick={() => this.onPageClick(false)}>⇐</div>
+                        <div><input className={s.input_current_page} type="text" value={current} onChange={() => alert('x')} /></div>
+                        <div className={current == total ? s.block : s.btn_next} onClick={() => this.onPageClick(true)}>⇒</div>
+                    </div>
+                </div>
                 <div className={s.box}>{mapUser}</div>
             </div>
         )
