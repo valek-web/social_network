852ade2a0dac9b216e7403f60bda5df58849ea31
valek-web <ziryanovvalentin@mail.ru>
2021-11-13 23:57:19 +0300

    testPtofileNewTest

--- src/redux/reducer/profile_reducer.test.js
+++ src/redux/reducer/profile_reducer.test.js
@@ -1,14 +1,14 @@
-import {addPostAC, profile_reducer} from './profile_reducer'
+import { addPostAC, setProfileInfoAC, profile_reducer } from './profile_reducer'
 
 it('new post', () => {
     //
-    const action = addPostAC()
+    const action = addPostAC('test')
     let State = {
         post: [
-            {message: 'New post!', like: 2, id: 0},
-            {message: 'I\'m learn Java!', like: 5, id: 1},
-            {message: 'I\'m learn Python!', like: 33, id: 2},
-            {message: 'I want programmes!', like: 15, id: 3},
+            { message: 'New post!', like: 2, id: 0 },
+            { message: "I'm learn Java!", like: 5, id: 1 },
+            { message: "I'm learn Python!", like: 33, id: 2 },
+            { message: 'I want programmes!', like: 15, id: 3 },
         ],
     }
 
@@ -17,3 +17,17 @@ it('new post', () => {
     //
     expect(newState.post.length).toBe(5)
 })
+
+// it('new post', () => {
+//     //
+//     const action = setProfileInfoAC('profile info')
+//     let State = {
+//         profile: null,
+//         boolDate: false,
+//     }
+
+//     //
+//     let newState = profile_reducer(State, action)
+//     //
+//     expect(newState.profile).toBe('profile info')
+// })
