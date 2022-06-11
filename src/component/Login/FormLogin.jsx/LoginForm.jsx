57f11e227d3d9f3ad2b64849045a7b1e48a7d92f
Valek <ziryanovvalentin@mail.ru>
2022-06-11 15:50:11 +0300

    deployDesktop

--- src/component/Login/FormLogin.jsx/LoginForm.jsx
+++ src/component/Login/FormLogin.jsx/LoginForm.jsx
@@ -6,41 +6,52 @@ import style from './LoginForm.module.css'
 
 const LoginForm = ({ handleSubmit, error, captcha }) => {
     return (
-        <form className={style.boxForm} onSubmit={handleSubmit}>
-            <div>
-                <Field
-                    name='email'
-                    type='email'
-                    component={Texterea}
-                    placeholder='Login'
-                    validate={requiered}
-                />
-            </div>
-            <div>
-                <Field
-                    name='password'
-                    type='password'
-                    component={Texterea}
-                    placeholder='Password'
-                    validate={requiered}
-                />
-            </div>
-            <div>
-                <Field name='checkbox' type='checkbox' component='input' />
-                <span>remember me</span>
-            </div>
-            {!error ? '' : <div className={style.errorBox}>{error}</div>}
-            {!!captcha ? <div>
-                <div><img src={captcha} alt='#'/></div>
-                <Field name='captcha' type='text' component='input'/>
-            </div>: ''}
-            <div className={style.btn_box}>
-                <button>Log in</button>
-                <a href='https://social-network.samuraijs.com/signUp'>
-                    sign up
-                </a>
-            </div>
-        </form>
+        <>
+            Тестовые Email и Password:
+            <div>Email: free@samuraijs.com </div>
+            <div>Password: free</div>
+            <form className={style.boxForm} onSubmit={handleSubmit}>
+                <div>
+                    <Field
+                        name='email'
+                        type='email'
+                        component={Texterea}
+                        placeholder='Login'
+                        validate={requiered}
+                    />
+                </div>
+                <div>
+                    <Field
+                        name='password'
+                        type='password'
+                        component={Texterea}
+                        placeholder='Password'
+                        validate={requiered}
+                    />
+                </div>
+                <div>
+                    <Field name='checkbox' type='checkbox' component='input' />
+                    <span>remember me</span>
+                </div>
+                {!error ? '' : <div className={style.errorBox}>{error}</div>}
+                {!!captcha ? (
+                    <div>
+                        <div>
+                            <img src={captcha} alt='#' />
+                        </div>
+                        <Field name='captcha' type='text' component='input' />
+                    </div>
+                ) : (
+                    ''
+                )}
+                <div className={style.btn_box}>
+                    <button className='button'>Log in</button>
+                    <a href='https://social-network.samuraijs.com/signUp'>
+                        sign up
+                    </a>
+                </div>
+            </form>
+        </>
     )
 }
 
